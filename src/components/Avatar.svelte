b4b9c063e9b2cc8f317749fc5e5690d54c95a4f5
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-07-31 23:44:58 +0300

    Show user's avatar in `Header`

---
+++ src/components/Avatar.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  export let avatar: string;
+  export let size = 32;
+</script>
+
+<img src={avatar} alt="" width={size} height={size} class="rounded-full" />
--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
   import { goto } from "$app/navigation";
   import { page } from "$app/stores";
+  import Avatar from "./Avatar.svelte";
 
   const isLoggedIn = $page.url.pathname !== "/login";
 
@@ -17,9 +18,14 @@
 <header class="m-2 flex justify-between items-center gap-2">
   <h1 class="text-5xl">fileasy</h1>
   {#if isLoggedIn}
-    <button
-      class="border-2 border-neutral-300 text-neutral-700 rounded-md py-1 px-2 hover:shadow-md"
-      on:click={handleLogout}>Logout</button
-    >
+    <div class="flex items-center gap-2">
+      {#if $page.data.user.avatar}
+        <Avatar avatar={$page.data.user.avatar} size={40} />
+      {/if}
+      <button
+        class="border-2 border-neutral-300 text-neutral-700 rounded-md py-1 px-2 hover:shadow-md"
+        on:click={handleLogout}>Logout</button
+      >
+    </div>
   {/if}
 </header>
