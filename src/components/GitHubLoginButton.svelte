4da9416e37743fa97e80f2a78367f576a10f2ce5
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-08-01 09:23:01 +0300

    Make login prettier
    still not ideal but good enough

---
+++ src/components/GitHubLoginButton.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { PUBLIC_API_URL } from "$env/static/public";
+  import GitHubLogo from "./GitHubLogo.svelte";
+</script>
+
+<a
+  href={`${PUBLIC_API_URL}/api/auth/github`}
+  class="flex items-center gap-2 bg-black rounded-lg py-2 px-4 text-white outline-offset-1 outline-neutral-800/60 hover:outline"
+>
+  <GitHubLogo size={24} variant="light" />
+  Login with GitHub
+</a>
---
+++ src/components/YandexLoginButton.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { PUBLIC_API_URL } from "$env/static/public";
+  import YandexLogo from "./YandexLogo.svelte";
+</script>
+
+<a
+  href={`${PUBLIC_API_URL}/api/auth/yandex`}
+  class="flex items-center gap-2 bg-yandex rounded-lg py-2 px-4 text-white outline-offset-1 outline-yandex/60 hover:outline"
+>
+  <YandexLogo size={24} />
+  Login with Yandex
+</a>
