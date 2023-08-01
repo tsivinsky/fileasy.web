783955e56e44d49e5c1fd061140ac9d00854e20d
Daniil Tsivinsky <daniil@tsivinsky.com>
2023-08-01 09:28:28 +0300

    Add margin around home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -44,35 +44,37 @@
   };
 </script>
 
-<form
-  on:submit|preventDefault={handleUploadFile}
-  class="flex flex-col items-start gap-2 mt-10"
->
-  <input
-    bind:value={newFileName}
-    class="border border-neutral-400 py-1 px-2 rounded-md outline-offset-1"
-    placeholder="File name"
-  />
-  <button
-    type="submit"
-    class="border border-neutral-400 hover:bg-neutral-200 text-neutral-700 py-1 px-3 rounded-md transition-colors duration-200"
-    >Upload file</button
+<div class="m-2">
+  <form
+    on:submit|preventDefault={handleUploadFile}
+    class="flex flex-col items-start gap-2 mt-10"
   >
-</form>
+    <input
+      bind:value={newFileName}
+      class="border border-neutral-400 py-1 px-2 rounded-md outline-offset-1"
+      placeholder="File name"
+    />
+    <button
+      type="submit"
+      class="border border-neutral-400 hover:bg-neutral-200 text-neutral-700 py-1 px-3 rounded-md transition-colors duration-200"
+      >Upload file</button
+    >
+  </form>
 
-<h2 class="text-3xl mt-2">Your files</h2>
-<div>
-  {#each data.user.files as file (file.id)}
-    <div class="group flex items-center gap-2">
-      <a href="/{file.name}" class="hover:underline hover:text-blue-500"
-        >{file.name}</a
-      >
-      <button
-        class="hidden group-hover:block"
-        on:click={() => handleDeleteFile(file.id)}
-      >
-        <TrashIcon size={18} color="#b00420" />
-      </button>
-    </div>
-  {/each}
+  <h2 class="text-3xl mt-2">Your files</h2>
+  <div>
+    {#each data.user.files as file (file.id)}
+      <div class="group flex items-center gap-2">
+        <a href="/{file.name}" class="hover:underline hover:text-blue-500"
+          >{file.name}</a
+        >
+        <button
+          class="hidden group-hover:block"
+          on:click={() => handleDeleteFile(file.id)}
+        >
+          <TrashIcon size={18} color="#b00420" />
+        </button>
+      </div>
+    {/each}
+  </div>
 </div>
